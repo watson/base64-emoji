3e722b902f7e82ffd9245c03aaf99c1592cf65df
Thomas Watson Steen <w@tson.dk>
2015-11-22 00:27:53 -0800

    Code cleanup: Make it clear that the emoji byte size is a constant

--- index.js
+++ index.js
@@ -2,20 +2,20 @@
 
 var mime = require('./mime')
 var emojis = require('./emojis')
-var emojiByteSize = 4
+var EMOJI_BYTE_SIZE = 4
 
 exports.encode = function (buf) {
   if (typeof buf === 'string') buf = new Buffer(buf)
   else if (!Buffer.isBuffer(buf)) throw new Error('Invalid argument! Expected string or Buffer')
 
   var base64 = buf.toString('base64')
-  var result = new Buffer(base64.length * emojiByteSize)
+  var result = new Buffer(base64.length * EMOJI_BYTE_SIZE)
   var index
 
   for (var n = 0; n < base64.length; n++) {
     index = mime.indexOf(base64[n])
     if (index === -1) throw new Error('Invalid MIME base64 character: ' + base64[n])
-    result.write(emojis[index], n * emojiByteSize)
+    result.write(emojis[index], n * EMOJI_BYTE_SIZE)
   }
 
   return result
@@ -25,12 +25,12 @@ exports.decode = function (buf) {
   if (typeof buf === 'string') buf = new Buffer(buf)
   else if (!Buffer.isBuffer(buf)) throw new Error('Invalid argument! Expected string or Buffer')
 
-  var emojisUsed = buf.length / emojiByteSize
+  var emojisUsed = buf.length / EMOJI_BYTE_SIZE
   var base64 = ''
   var emoji, index
 
   for (var n = 0; n < emojisUsed; n++) {
-    emoji = buf.slice(n * emojiByteSize, n * emojiByteSize + emojiByteSize).toString()
+    emoji = buf.slice(n * EMOJI_BYTE_SIZE, n * EMOJI_BYTE_SIZE + EMOJI_BYTE_SIZE).toString()
     index = emojis.indexOf(emoji)
     if (index === -1) throw new Error('Invalid base64-emoji character: ' + emoji)
     base64 += mime[index]
